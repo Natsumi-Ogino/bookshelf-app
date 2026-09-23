edde94aa629a1a349dd78451bc511eb06989f903
Natsumi-Ogino <273796069+Natsumi-Ogino@users.noreply.github.com>
2026-09-23 20:24:49 +0900

    ログイン用バリデーションメッセージをFormRequestに追加

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -13,4 +13,13 @@ class LoginRequest extends FortifyLoginRequest
 
         return $rules;
     }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'password.required' => 'パスワードを入力してください',
+        ];
+    }
 }
